80c6b4a902bb08d42f8eb68e5ef689b751c7b143
avtozapchastistore <avtozapchastistore@outlook.com>
2026-05-10 23:25:28 +0300

    Update 001_add_cancelled_status.php

--- 001_add_cancelled_status.php
+++ 001_add_cancelled_status.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/config.php';
 
 try {
     $pdo = new PDO("mysql:host={$host};port={$port};dbname={$name};charset=utf8mb4", $user, $pass);
